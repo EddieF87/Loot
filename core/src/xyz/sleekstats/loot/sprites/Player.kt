b020a42c504711ee63e43e2f9a65e25d2a29835e
Eddie Foy <edwardfoyiii@gmail.com>
2019-01-11 10:32:03 +1100

    Remove b2Body from Player class
    b2Body not needed for player. Also rename some fields/methods.

--- core/src/xyz/sleekstats/loot/sprites/Player.kt
+++ core/src/xyz/sleekstats/loot/sprites/Player.kt
@@ -2,11 +2,9 @@ package xyz.sleekstats.loot.sprites
 
 import com.badlogic.gdx.graphics.g2d.Sprite
 import com.badlogic.gdx.graphics.g2d.TextureRegion
-import com.badlogic.gdx.math.Vector2
 import com.badlogic.gdx.physics.box2d.*
 import xyz.sleekstats.loot.LootGame
 import xyz.sleekstats.loot.screens.PlayScreen
-import kotlin.experimental.or
 
 class Player(var world: World, val playScreen: PlayScreen) : Sprite(playScreen.textureAtlas.findRegion("little_mario")) {
 
@@ -14,28 +12,12 @@ class Player(var world: World, val playScreen: PlayScreen) : Sprite(playScreen.t
             0, 0, 16, 16)
     private var playerImg2 = TextureRegion(playScreen.textureAtlas.findRegion("goomba"),
             0, 0, 16, 16)
-    lateinit var b2Body: Body
-    var isMario = true
+    var isCollecting = false
 
     init {
-        defineMario()
         println("width = ${playScreen.viewport.worldWidth} height = ${playScreen.viewport.worldHeight} ")
         setBounds(playScreen.viewport.worldWidth / 2, playScreen.viewport.worldHeight / 4, 16F, 16F)
         setRegion(playerImg)
-    }
-
-    fun defineMario() {
-        val bdef = BodyDef()
-        bdef.position.set(LootGame.V_WIDTH /2, LootGame.V_HEIGHT /4)
-        bdef.type = BodyDef.BodyType.DynamicBody
-        b2Body = world.createBody(bdef)
-
-        val fdef = FixtureDef()
-        val shape = CircleShape()
-        shape.radius = 16F
-
-        fdef.shape = shape
-        b2Body.createFixture(fdef).userData = this
     }
 
     fun update(dt: Float) {
@@ -43,14 +25,14 @@ class Player(var world: World, val playScreen: PlayScreen) : Sprite(playScreen.t
     }
 
     fun transformPlayer() {
-        if(isMario) {
-            isMario = false
-            setRegion(playerImg2)
-            setPosition(LootGame.V_WIDTH /2, LootGame.V_HEIGHT /2)
-        } else {
-            isMario = true
+        if(isCollecting) {
+            isCollecting = false
             setRegion(playerImg)
             setPosition(LootGame.V_WIDTH /2, LootGame.V_HEIGHT /4)
+        } else {
+            isCollecting = true
+            setRegion(playerImg2)
+            setPosition(LootGame.V_WIDTH /2, LootGame.V_HEIGHT /2)
         }
     }
 }
